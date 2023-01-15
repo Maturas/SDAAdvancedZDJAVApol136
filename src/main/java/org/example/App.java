f568e7e18043c3615e5444e0cf41b925dd1b3d94
Mateusz Zawistowski <maturasisko@gmail.com>
2023-01-15 10:04:31 +0100

    Added class and Enum examples

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.example.Classes.FoodType;
+import org.example.Classes.Human;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        FoodType.printExample();
+    }
+}
