180cc847859e018682e7bffb8167761eefbd9c39
Mateusz Zawistowski <maturasisko@gmail.com>
2023-01-15 11:55:48 +0100

    Added class relations examples

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,10 +1,7 @@
 package org.example;
 
+import org.example.ClassRelations.ManyToMany.Person;
 import org.example.Classes.FoodType;
-import org.example.Classes.Human;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Hello world!
@@ -13,6 +10,6 @@ import java.util.List;
 public class App 
 {
     public static void main( String[] args ) {
-        FoodType.printExample();
+        Person.example();
     }
 }
