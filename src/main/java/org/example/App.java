3bd4c93f403b35b877215939033f84d69a283dab
Mateusz Zawistowski <maturasisko@gmail.com>
2023-02-04 15:28:20 +0100

    Added basic multithreading example

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,7 @@
 package org.example;
 
-import org.example.Reflection.ReflectionExamples;
-import org.example.Reflection.VideoGame;
-
 public class App {
     public static void main( String[] args ) {
-        VideoGame game = new VideoGame("Crysis", "Crytek", "Shooter", 2007);
-        ReflectionExamples.getValueOfAPrivateField(game, "title", String.class);
-        ReflectionExamples.setValueOfAPrivateField(game, "title", "Killzone");
-        ReflectionExamples.getValueOfAPrivateField(game, "title", String.class);
+
     }
 }
