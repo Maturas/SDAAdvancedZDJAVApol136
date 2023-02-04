56f4f8a032173c857efeb2f21acdd0e5e4ec1226
Mateusz Zawistowski <maturasisko@gmail.com>
2023-02-04 16:12:41 +0100

    Added deadlock example

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,10 @@
 package org.example;
 
-public class App {
-    public static void main( String[] args ) {
+import org.example.Multithreading.DeadlockExample;
+import org.example.Multithreading.SynchronizedCounter;
 
+public class App {
+    public static void main( String[] args ) throws InterruptedException {
+        DeadlockExample.example();
     }
 }
